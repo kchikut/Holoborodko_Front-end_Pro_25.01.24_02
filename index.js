6f9452045f02ed3a48e4d2446289d058519bee78
unknown <you@example.com>
2024-02-02 11:58:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function isSixDigitNumber(str) {
+    const regex = /^\s*[+-]?\d{6}\s*$/;
+    return regex.test(str);
+}
+
+console.log(isSixDigitNumber('   123456  '));
+console.log(isSixDigitNumber(' -0123456  '));
+console.log(isSixDigitNumber('     -12345  '));
+console.log(isSixDigitNumber('   -023456'));
+console.log(isSixDigitNumber(' -000016   '));
+console.log(isSixDigitNumber(' -000000123456  '));
+console.log(isSixDigitNumber(' - 000000123456  '));
+console.log(isSixDigitNumber(' +000123456  '));
